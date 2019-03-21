a3dac1d22a3caa116a941c170fa172e3233c150a
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-21 01:53:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   bbq = []
   holiday_hash.each do |season, holidays|
-   bbq >> holiday.select {|name, supply| supply.include?
+   bbq >> holiday.select {|name, supply| supply.include?("BBQ")
      holidays
 end
 end
